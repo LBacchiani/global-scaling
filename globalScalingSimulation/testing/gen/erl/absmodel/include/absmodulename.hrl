79bd49d52b0b8237d94828fa12ba7b3ba6f838be
lorenzo <lorenzo.bacchiani@studio.unibo.it>
2020-09-02 17:39:31 +0200

    remove useless file

--- globalScalingSimulation/testing/gen/erl/absmodel/include/absmodulename.hrl
+++
@@ -1,5 +0,0 @@
-%%This file is licensed under the terms of the Modified BSD License.
--undef(ABSMAINMODULE).
--define(ABSMAINMODULE,m_Test2).
--undef(ABSCOMPILERVERSION).
--define(ABSCOMPILERVERSION,"v1.9.0-35-g11cd5fa78").
