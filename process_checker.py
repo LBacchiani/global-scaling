4f1ebc50c081106e266c2691ee88ffd7190bac5f
LBacchiani <>
2022-01-11 21:32:11 +0100

    fix process_checker.py

--- process_checker.py
+++ process_checker.py
@@ -11,28 +11,24 @@ def read_file(filename):
         lines = f.readlines()
     return lines[-1]
 
-def kill_by_name(name1, name2, name3):
+def kill_by_name(name):
     for proc in psutil.process_iter():
-        if name1 in proc.name() or name2 in proc.name() or name3 in proc.name():
+        if proc.name() == name:
             proc.kill()
 
-def restart(total_run, name1, name2, name3):
+def restart(total_run):
     sys.argv[3] = str(total_run)
+    kill_by_name("beam.smp")
     os.execv(sys.executable, ['python3'] + sys.argv)
-    kill_by_name(name1, name2, name3)
     raise SystemExit
 
 def check(filename, commandPath, runs):
     total_run = runs
-
     if total_run < 100:
         if os.path.exists("ABS-test/" + filename + str(total_run) + ".dat"): os.remove("ABS-test/" + filename + str(total_run) + ".dat")
         p = subprocess.Popen(["ABS-Simulations-Comparison/" + commandPath + "/gen/erl/run > ABS-test/" + filename + str(total_run) + ".dat"], stdout=subprocess.PIPE, shell=True)
         print(commandPath + " PID " + str(p.pid) + " run number: " + str(total_run))
         current = ""
-        procName1 = "ABS-Simulations-Comparison/" + commandPath + "/gen/erl/run > ABS-test/" + filename + str(total_run) + ".dat"
-        procName2 = "ABS-Simulations-Comparison/" + commandPath + "/gen/erl/run
-        procName3 = "-B -- -root /usr/lib/erlang -progname erl -- -home /root -- -boot no_dot_erlang -noshell -run escript start -extra"
         while(True):
             time.sleep(600)
             last_line = read_file("ABS-test/" + filename + str(total_run) + ".dat")
@@ -41,11 +37,11 @@ def check(filename, commandPath, runs):
                 if t >= 220:
                     print("Exec number " + str(total_run) + " of " + commandPath + " ended")
                     total_run += 1
-                    restart(total_run, p)
+                    restart(total_run)
                 current = last_line
             else:
-                print("Restart exec number " + str(total_run) + " of " + commandPath + " ended")
-                restart(total_run, p)
+                print("Restart exec number " + str(total_run) + " of " + commandPath)
+                restart(total_run)
 
 
 if not os.path.exists("ABS-test"): os.makedirs("ABS-test")
