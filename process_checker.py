0d52029920371f12b7855dcdfaa430f409639491
LBacchiani <>
2022-01-11 20:53:37 +0100

    fix

--- process_checker.py
+++ process_checker.py
@@ -11,19 +11,28 @@ def read_file(filename):
         lines = f.readlines()
     return lines[-1]
 
-def restart(total_run, process):
+def kill_by_name(name1, name2, name3):
+    for proc in psutil.process_iter():
+        if name1 in proc.name() or name2 in proc.name() or name3 in proc.name():
+            proc.kill()
+
+def restart(total_run, name1, name2, name3):
     sys.argv[3] = str(total_run)
     os.execv(sys.executable, ['python3'] + sys.argv)
-    os.killpg(os.getpgid(process.pid), signal.SIGTERM)
+    kill_by_name(name1, name2, name3)
     raise SystemExit
 
 def check(filename, commandPath, runs):
     total_run = runs
+
     if total_run < 100:
         if os.path.exists("ABS-test/" + filename + str(total_run) + ".dat"): os.remove("ABS-test/" + filename + str(total_run) + ".dat")
         p = subprocess.Popen(["ABS-Simulations-Comparison/" + commandPath + "/gen/erl/run > ABS-test/" + filename + str(total_run) + ".dat"], stdout=subprocess.PIPE, shell=True)
         print(commandPath + " PID " + str(p.pid) + " run number: " + str(total_run))
         current = ""
+        procName1 = "ABS-Simulations-Comparison/" + commandPath + "/gen/erl/run > ABS-test/" + filename + str(total_run) + ".dat"
+        procName2 = "ABS-Simulations-Comparison/" + commandPath + "/gen/erl/run
+        procName3 = "-B -- -root /usr/lib/erlang -progname erl -- -home /root -- -boot no_dot_erlang -noshell -run escript start -extra"
         while(True):
             time.sleep(600)
             last_line = read_file("ABS-test/" + filename + str(total_run) + ".dat")
